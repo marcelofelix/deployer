7ee53223f5ac8d835d5155336b187f9cfbab8a43
Marcelo Felix <olivera.felix@gmail.com>
2017-01-26 15:01:41 -0200

    remove auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   unless Rails.env.test?
-    http_basic_authenticate_with name: ENV['USER'], password: ENV['PASSWORD']
+    # http_basic_authenticate_with name: ENV['USER'], password: ENV['PASSWORD']
   end
 end
